16c6b2fa115a08583b8002ea48c0b988f9c7cff2
fblanc <frederic.blanc@coservit.fr>
2017-09-19 14:15:09 +0200

    Premier test

---
+++ myWebApp/src/components/Cv.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Here will be the CV :)</h1>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+    .list {
+        width: 100%;
+        padding: 0;
+    }
+</style>
